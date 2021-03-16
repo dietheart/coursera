cc848549ec73804bf9fa371a52165878f300d26e
Emanuel <emanuelrochamgwbih@gmail.com>
2021-03-16 16:52:24 -0300

    no biggie

---
+++ jscourse/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+    if (vel> 60) {console.log('voce ultrapassou a velocidade permitida! MULTADO!')}
+    else{console.log('Preste Atencao!')}
+
+console.log('Use cinto de seguranca!')
